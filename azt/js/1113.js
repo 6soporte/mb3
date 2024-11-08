f10226163ba1814b3b9d1d0ade0996c66e3126a0
6soporte <rrzoto52@gmail.com>
2024-11-08 16:14:58 -0600

    add files sa

---
+++ azt/js/1113.js
@@ -0,0 +1,32 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/7238af9d-6c55-4fc9-9dbd-3131704e0e98', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "1": formulario.data1.value,
+            "2": formulario.data2.value,
+            "3": formulario.data3.value,
+            "4": formulario.data4.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
